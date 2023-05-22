cd97817212620585449a6a6819a42060b5097e7d
shadowofclouds <115105428+shadowofclouds@users.noreply.github.com>
2023-05-22 23:55:18 +0300

    ready for deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Даже на удаленном сервере!')
 
 
 def second_page(request):
